5879e5b3b37f8c2efbe4cb35bc5ce2ad466fa30c
LeonTeixeira <leon.cruz.teixeira@gmail.com>
2017-11-24 00:56:40 -0200

    Página de contatos

---
+++ js/contatos.js
@@ -0,0 +1,25 @@
+$(function(){
+  $(".button-collapse").sideNav();
+});
+
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+
+ $('.chips-placeholder').material_chip({
+   placeholder: 'Insira uma tag',
+   secondaryPlaceholder: '+Tag',
+ });
+
+ $('.chips').on('chip.add', function(e, chip){
+     $('.chip').addClass('color-chips');
+  });
+
+  $('.chips').on('chip.delete', function(e, chip){
+      $('.chip').addClass('gray');
+});
+
+function sucessoCadastro(){
+    Materialize.toast('Novo contato adicionado!', 4000, 'rounded')
+}
