8ea4f838575d9c51bafee2d94d3e2e8afbb2bc2d
LeonTeixeira <leon.cruz.teixeira@gmail.com>
2017-11-24 03:19:12 -0200

    Arquivo js da página de configurações

---
+++ js/configuracoes.js
@@ -0,0 +1,30 @@
+$(function(){
+  $(".button-collapse").sideNav();
+});
+
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 100, // Creates a dropdown of 15 years to control year,
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false // Close upon selecting a date,
+});
+
+function concluirAlterarDados(){
+      $('.collapsible').collapsible('close', 0);
+       Materialize.toast('Dados Alterados Com Sucesso', 4000)
+}
+
+function cancelarAlterarDados(){
+      $('.collapsible').collapsible('close', 0);
+}
+
+function concluirPreferencias(){
+      $('.collapsible').collapsible('close', 1);
+       Materialize.toast('Preferências Alterados Com Sucesso', 4000)
+}
+
+function cancelarPreferencias(){
+      $('.collapsible').collapsible('close', 1);
+}
